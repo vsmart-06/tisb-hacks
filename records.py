ca5a42aa1254deefbcb76a1b8a2ca80ead84be0c
vsmart-06 <srivishnuvusirikala@gmail.com>
2023-01-28 17:02:05 +0530

    Added a small fix

--- records.py
+++ records.py
@@ -9,28 +9,14 @@ c.execute('''
         password TEXT NOT NULL
     )
 ''')
-c.execute('''
-    CREATE TABLE IF NOT EXISTS events (
-        id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-        username TEXT NOT NULL,
-        title STRING NOT NULL,
-        description STRING,
-        date STRING NOT NULL,
-        time STRING NOT NULL,
-        location STRING,
-        attendees STRING
-    )
-''')
 conn.commit()
 
 def user_login(username, password):
     conn = db.connect("tisb-hacks/tisb-hacks.db")
     c = conn.cursor()
-    c.execute(f"SELECT * FROM user_credentials WHERE username = '{username}'")
+    c.execute(f"SELECT * FROM user_credentials WHERE username = '{username}' AND password = '{password}'")
     record = c.fetchone()
     if not record:
-        return False
-    elif password != record[1]:
         c.close()
         conn.close()
         return False
