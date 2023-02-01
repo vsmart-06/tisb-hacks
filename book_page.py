88894492410af8c6772311aac1ed39820c4b8a5c
vsmart-06 <srivishnuvusirikala@gmail.com>
2023-02-01 20:16:43 +0530

    Added more functionality to booking page

--- book_page.py
+++ book_page.py
@@ -17,6 +17,7 @@ class Book:
         self.username = username
 
         self.window = tk.Tk()
+        self.window.title("Book")
         self.window.tk.call("source", "./tisb-hacks/azure.tcl")
         self.window.tk.call("set_theme", "dark")
         self.window.geometry("1200x400")
@@ -41,45 +42,84 @@ class Book:
 
         self.window.mainloop()
     
-    def search_place(self):
-        place_string = self.drop_ent.get()
+    def search_place(self, pickup: bool = False):
+        if not pickup:
+            place_string = self.drop_ent.get()
 
-        places = requests.get(f"https://maps.googleapis.com/maps/api/place/autocomplete/json?input={place_string}&key={API_KEY}").json()["predictions"]
-        place_names = [x["description"] for x in places]
+            places = requests.get(f"https://maps.googleapis.com/maps/api/place/autocomplete/json?input={place_string}&key={API_KEY}").json()["predictions"]
+            place_names = [x["description"] for x in places]
 
-        if len(place_names) > 5:
-            place_names = place_names[:5]
+            if len(place_names) > 5:
+                place_names = place_names[:5]
 
-        try:
-            self.places_frame_start.destroy()
-        except:
-            pass
+            try:
+                self.places_frame_start.destroy()
+            except:
+                pass
 
-        self.places_frame_start = ttk.Frame(self.drop_frame)
-        self.places_frame_start.grid(row = 1, column = 1, sticky = "ew")
-        self.places_btns_start = []
+            self.places_frame_start = ttk.Frame(self.drop_frame)
+            self.places_frame_start.grid(row = 1, column = 1, sticky = "ew")
+            self.places_btns_start = []
 
-        for x in range(len(place_names)):
-            self.places_btns_start.append(ttk.Button(self.places_frame_start, text = place_names[x], command = lambda m = x: self.choose_place(m)))
-            self.places_btns_start[-1].grid(row = x, column = 0, pady = (0, 5), sticky = "ew")
-        
-        self.window.update()
-        self.sidebar = Sidebar(self.window, self.username)
+            for x in range(len(place_names)):
+                self.places_btns_start.append(ttk.Button(self.places_frame_start, text = place_names[x], command = lambda m = x: self.choose_place(m)))
+                self.places_btns_start[-1].grid(row = x, column = 0, pady = (0, 5), sticky = "ew")
+            
+            self.window.update()
+            self.sidebar = Sidebar(self.window, self.username)
+        else:
+            place_string = self.pickup_ent.get()
+
+            places = requests.get(f"https://maps.googleapis.com/maps/api/place/autocomplete/json?input={place_string}&key={API_KEY}").json()["predictions"]
+            place_names = [x["description"] for x in places]
+
+            if len(place_names) > 5:
+                place_names = place_names[:5]
+
+            try:
+                self.places_frame_start.destroy()
+            except:
+                pass
+
+            self.places_frame_start = ttk.Frame(self.pickup_frame)
+            self.places_frame_start.grid(row = 1, column = 1, sticky = "ew")
+            self.places_btns_start = []
+
+            for x in range(len(place_names)):
+                self.places_btns_start.append(ttk.Button(self.places_frame_start, text = place_names[x], command = lambda m = x, n = True: self.choose_place(m, n)))
+                self.places_btns_start[-1].grid(row = x, column = 0, pady = (0, 5), sticky = "ew")
+            
+            self.pickup_window.update()
+            self.sidebar = Sidebar(self.pickup_window, self.username)
     
-    def choose_place(self, index):
-        self.chosen_place = self.places_btns_start[index]["text"]
-        self.drop_ent.delete(0, "end")
-        self.drop_ent.insert(0, self.chosen_place)
-        try:
-            self.places_frame_start.destroy()
-        except:
-            pass
+    def choose_place(self, index, pickup: bool = False):
+        if not pickup:
+            self.chosen_place = self.places_btns_start[index]["text"]
+            self.drop_ent.delete(0, "end")
+            self.drop_ent.insert(0, self.chosen_place)
+            try:
+                self.places_frame_start.destroy()
+            except:
+                pass
+        else:
+            self.chosen_place = self.places_btns_start[index]["text"]
+            self.pickup_ent.delete(0, "end")
+            self.pickup_ent.insert(0, self.chosen_place)
+            try:
+                self.places_frame_start.destroy()
+            except:
+                pass
+            self.create_map()
     
     def find_drivers(self):
-        self.window.destroy()
-        self.sub_window = tk.Tk()
-        self.sub_window.tk.call("source", "./tisb-hacks/azure.tcl")
-        self.sub_window.tk.call("set_theme", "dark")
+        try:
+            self.sub_main_frame.destroy()
+        except:
+            self.window.destroy()
+            self.sub_window = tk.Tk()
+            self.sub_window.title("Book")
+            self.sub_window.tk.call("source", "./tisb-hacks/azure.tcl")
+            self.sub_window.tk.call("set_theme", "dark")
 
         self.sub_main_frame = ttk.Frame(self.sub_window)
         self.sub_main_frame.grid(row = 0, column = 1)
@@ -99,18 +139,82 @@ class Book:
             self.maps.append(ImageTk.PhotoImage(Image.open(f"./tisb-hacks/assets/map_route_{x}.png")))
             self.maps_lbls.append(ttk.Label(self.maps_frame, image = self.maps[x]))
             self.maps_lbls[x].grid(row = x, column = 0, padx = 20, pady = 20)
-            self.maps_select.append(ttk.Button(self.maps_frame, text = "Select", style = "Accent.TButton", command = lambda m = x: self.select_map(m)))
+            self.maps_select.append(ttk.Button(self.maps_frame, text = "Select", style = "Accent.TButton", command = lambda m = x, n = options[x][0]: self.select_map(m, n)))
             self.maps_select[x].grid(row = x, column = 1, padx = 20, pady = (0, 10))
         
         self.sub_window.update()
         self.sidebar = Sidebar(self.sub_window, self.username)
     
-    def select_map(self, index):
+    def select_map(self, index, id):
+        self.chosen_index = index
         self.maps_frame.destroy()
         self.maps_frame = ttk.Frame(self.sub_main_frame)
-        self.maps_frame.grid(row = 0, column = 0)
+        self.maps_frame.grid(row = 1, column = 0)
+
+        self.data = get_lifts(id)
+
+        distances = requests.get(f"https://maps.googleapis.com/maps/api/distancematrix/json?origins={self.chosen_place.replace(' ', '%20')}&destinations={self.data[2].replace(' ', '%20')}|{self.data[3].replace(' ', '%20')}&mode=walking&key={API_KEY}").json()
+
+        distances = [x["distance"]["value"] for x in distances["rows"][0]["elements"]]
+        
+        self.confirmation = ttk.Label(self.sub_main_frame, text = f"Details of the journey:\n\n\nStarting point (point A): {self.data[2]}\n\nDestination (point B): {self.data[3]}\n\nCarpooler: {self.data[1]}\n\nDistance of your destination from point A: {distances[0]/1000}km\nDistance of your destination from point B: {distances[1]/1000}km", justify = "center")
+        self.confirmation.grid(row = 0, column = 0, padx = 10, pady = 10)
 
         self.chosen_map = ImageTk.PhotoImage(Image.open(f"./tisb-hacks/assets/map_route_{index}.png"))
-        self.chosen_index = index
         self.chosen_map_lbl = ttk.Label(self.maps_frame, image = self.chosen_map)
         self.chosen_map_lbl.grid(row = 0, column = 0, padx = 20, pady = 20)
+
+        self.btns_frame = ttk.Frame(self.sub_main_frame)
+        self.btns_frame.grid(row = 2, column = 0, padx = 10, pady = 10)
+
+        self.confirm_btn = ttk.Button(self.btns_frame, text = "Book the ride", style = "Accent.TButton", command = self.confirm_ride)
+        self.confirm_btn.grid(row = 0, column = 1, padx = 10, pady = 10)
+
+        self.back_btn = ttk.Button(self.btns_frame, text = "Back", command = self.find_drivers)
+        self.back_btn.grid(row = 0, column = 0, padx = 10, pady = 10)
+
+        self.sub_window.update()
+        self.sidebar = Sidebar(self.sub_window, self.username)
+    
+    def confirm_ride(self):
+        self.sub_window.destroy()
+        self.pickup_window = tk.Tk()
+        self.pickup_window.title("Book")
+        self.pickup_window.tk.call("source", "./tisb-hacks/azure.tcl")
+        self.pickup_window.tk.call("set_theme", "dark")
+
+        self.pickup_main_frame = ttk.Frame(self.pickup_window)
+        self.pickup_main_frame.grid(row = 0, column = 1, padx = 10, pady = 10)
+
+        self.pickup_frame = ttk.Frame(self.pickup_main_frame)
+        self.pickup_frame.grid(row = 0, column = 0, padx = 10, pady = 10)
+
+        self.pickup_lbl = ttk.Label(self.pickup_frame, text = "Enter your pickup point")
+        self.pickup_lbl.grid(row = 0, column = 0, padx = 10, pady = 10)
+        self.pickup_ent = ttk.Entry(self.pickup_frame)
+        self.pickup_ent.grid(row = 0, column = 1, padx = 10, pady = 10)
+        self.pickup_btn = ttk.Button(self.pickup_frame, text = "Search", command = lambda m = True: self.search_place(m))
+        self.pickup_btn.grid(row = 0, column = 2, padx = 10, pady = 10)
+
+        self.map_frame = ttk.Frame(self.pickup_main_frame)
+        self.map_frame.grid(row = 1, column = 0)
+
+        self.book_btn = ttk.Button(self.pickup_main_frame, text = "Book Ride", style = "Accent.TButton", command = self.book_ride)
+        self.book_btn.grid(row = 2, column = 0, padx = 10, pady = 10)
+
+        self.pickup_window.update()
+        self.sidebar = Sidebar(self.pickup_window, self.username)
+    
+    def create_map(self):
+        self.map_url = f"https://maps.googleapis.com/maps/api/staticmap?size=300x300&markers=color:blue|label:A|{self.data[2].replace(' ', '%20')}&markers=color:blue|label:B|{self.data[3].replace(' ', '%20')}&markers=color:red|{self.chosen_place.replace(' ', '%20')}&path=enc:{self.data[4]}&key={API_KEY}"
+        request.urlretrieve(self.map_url, f"./tisb-hacks/assets/map_route_pickup.png")
+
+        self.map = ImageTk.PhotoImage(Image.open(f"./tisb-hacks/assets/map_route_pickup.png"))
+        self.maps_lbl = ttk.Label(self.map_frame, image = self.map)
+        self.maps_lbl.grid(row = 0, column = 0, padx = 20, pady = 20)
+    
+    def book_ride(self):
+        pass
+
+
+Book("vishnu")
